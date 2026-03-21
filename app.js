1901cc3b06fb6e9fc73a9b2523f1fd42bb8a6b0d
Md Sadeq <sadiq02999@gmail.com>
2026-03-21 18:34:46 +0530

    added current path middleware

--- app.js
+++ app.js
@@ -77,7 +77,9 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currUser = req.user;  next();
+  res.locals.currUser = req.user;
+  res.locals.currentPath = req.path;
+  next();
 });
 
 ////ROUTES
